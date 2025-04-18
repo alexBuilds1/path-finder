fa557d253246d1055e0cd940ad0971da4a6dfa06
Alex Clean <alexcleanbuilds@gmail.com>
2025-04-18 15:38:31 +0530

    pages.dev cloudflare pages check

---
+++ functions/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>☁️ Cloudflare Pages + React</h1>
+      <p>This is rendered by React (App.jsx)</p>
+    </div>
+  );
+}
